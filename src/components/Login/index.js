943fd185fb4691e80d92dede27cfa77d0962782e
reza <rezza.op@gmail.com>
2021-07-08 20:33:55 +0430

    add farsi to login and register

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,64 +2,73 @@ import React, { useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import styles from "../../styles/Login.module.css"
 import { auth } from "../../firebase"
+import { useTranslation } from "react-i18next";
+import { Col, Row } from 'antd';
+import { ConfigProvider } from 'antd';
+
 function Login() {
+    const { t, i18n } = useTranslation()
     const history = useHistory();
+    const lang = i18n.language
     const [err, seterr] = useState('')
     const login = (e) => {
         e.preventDefault();
-         const input_email =  e.target[0].value
+        const input_email = e.target[0].value
         const input_password = e.target[1].value
         if (input_email && input_password) {
             auth.signInWithEmailAndPassword(input_email, input_password)
                 .then(auth => {
                     history.push('/')
                 })
-                .catch(err => seterr("Email or password is incorrect !"))
+                .catch(err => seterr(t("err_login1")))
         }
         else {
-            seterr(" Email or password is empty ! ")
+            seterr(t("err_login2"))
         }
     }
     return (
-        <div className={styles.login}>
-            <Link to='/'>
-                <p className={styles.logo}>9TuBe</p>
-            </Link>
-            <div className={styles.login_container}>
-                <h4 className={styles.sign}>Sign-in</h4>
-                <form className={styles.form} onSubmit={login}>
-                    <p className={styles.label}>Email </p>
-                    <input
-                        type="email"
-                        className={styles.input}
-                        placeholder="Enter your email"
-                        required
-                    />
-                    <p className={styles.label}>Password </p>
-                    <input
-                        type="password"
-                        className={styles.input}
-                        placeholder='Enter your password '
-                        required
-                    />
-                    <p className={styles.err}>
-                        {err}
-                    </p>
-                    <button
-                        type='submit'
-                        className={styles.btn}
-                    >Sign in</button>
-                </form>
-                <p className={styles.text}>
-                    if you dont have account click on
-
-                    <Link to="/register" className={styles.link}>
-                        create your acccount
+        <ConfigProvider direction={lang === "fa" ? "rtl" : "ltr"}>
+            <Row className={styles.row}>
+                <Col span={24} className={styles.login}>
+                    <Link to='/'>
+                        <p className={styles.logo}>9TuBe</p>
                     </Link>
+                    <div className={styles.login_container}>
+                        <h4 className={styles.sign}>{t("sign_in")}</h4>
+                        <form className={styles.form} onSubmit={login}>
+                            <p className={styles.label}> {t("email")} </p>
+                            <input
+                                type="email"
+                                className={styles.input}
+                                placeholder={t("email_place")}
+                                required
+                            />
+                            <p className={styles.label}>{t("password")} </p>
+                            <input
+                                type="password"
+                                className={styles.input}
+                                placeholder={t("password_place")}
+                                required
+                            />
+                            <p className={styles.err}>
+                                {err}
+                            </p>
+                            <button
+                                type='submit'
+                                className={styles.btn}
+                            >{t("sign_in")}</button>
+                        </form>
+                        <p className={styles.text}>
+                            {t("login_text")}
+                            <Link to="/register" className={styles.link}>
+                               {t("create_acc")}
+                            </Link>
 
-                </p>
-            </div>
-        </div>
+                        </p>
+                    </div>
+                </Col>
+            </Row>
+        </ConfigProvider>
     )
 }
 

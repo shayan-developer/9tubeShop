80f06c684fda00a86b16b26ce80802f2b1f80ddd
reza <rezza.op@gmail.com>
2021-07-08 21:17:02 +0430

    fix bugs

--- src/components/CheckoutProduct/index.js
+++ src/components/CheckoutProduct/index.js
@@ -4,15 +4,17 @@ import { useStateValue } from '../Context/StateProvider'
 import { useTranslation } from "react-i18next";
 function CheckoutProduct({ img, id, title, price }) {
     const state = useStateValue()
-    const dispath=state[1]
+    const dispath = state[1]
     const { t } = useTranslation()
-    const removing = () => {
+
+    const removing = (e) => {
         dispath({
             type: "REMOVE", id
         })
     }
+ 
     return (
-        <div className={styles.checkoutProduct}>
+        <div className={styles.checkoutProduct} >
             <img src={img} alt={title} className={styles.img} />
             <div className={styles.info}>
                 <p className={styles.title}>{title}</p>

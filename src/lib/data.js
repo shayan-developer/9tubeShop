f08f415618d7883e820b8f1c52b52db18e5cfe2e
shayan-developer <shayan.dev.arak@gmail.com>
2021-08-07 18:11:40 +0430

    changing photo

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,74 +1,89 @@
+import redDaul from "../assets/red-dual.png"
+import allStar from "../assets/allStar.png"
+import camera from "../assets/camera.png"
+import charging from "../assets/charging.png"
+import Console from "../assets/console.png"
+import ghost from "../assets/ghost.png"
+import headPhone from "../assets/headPhone.png"
+import mlb from "../assets/mlb.png"
+import ratchet from "../assets/ratchet.png"
+import returnal from "../assets/returnal.png"
+import spider from "../assets/spider.png"
+import whitedual from "../assets/white-dual.png"
+
+
+
 export const data = [
     {
         id: "1234567",
         title: "DualSense™ Wireless Controller",
-        img: "https://m.media-amazon.com/images/I/61iWP0ViIKL._AC_SL1500_.jpg",
+        img: redDaul,
         price: 110
     },
     {
         id: "2234567",
         title: "PULSE 3D™ Wireless Headset",
-        img: "https://images-na.ssl-images-amazon.com/images/I/41HQbvhq8AL._SX300_SY300_QL70_FMwebp_.jpg",
+        img: headPhone,
         price: 99
     },
     {
         id: "3234567",
         title: "DualSense™ Wireless Controller",
-        img: "https://m.media-amazon.com/images/I/61rzqjtJo2L._SL1460_.jpg",
+        img: whitedual,
         price: 69
     },
     {
         id: "6234567",
         title: "The PS5™ console ",
-        img: "https://m.media-amazon.com/images/I/61JbCra+7GL._SL1500_.jpg",
+        img: Console,
         price: 916
     },
     {
         id: "6234567",
         title: "HD Camera for PS5 consoles ",
-        img: "https://m.media-amazon.com/images/I/61NEypMstRL._SL1500_.jpg",
+        img: camera,
         price: 59
     },
     {
         id: "6234567",
         title: "DualSense™ Charging Station ",
-        img: "https://m.media-amazon.com/images/I/71ywUrrVHFL._SL1500_.jpg",
+        img: charging,
         price: 29
     },
     {
         id: "6234567",
-        title: "Assassin’s Creed Valhalla",
-        img: "https://m.media-amazon.com/images/I/81q2v4mcZSL._SL1500_.jpg",
+        title: "Ghost of tsushima",
+        img: ghost,
         price: 69
     },
     {
         id: "6234567",
         title: "Destruction AllStars - PS5",
-        img: "https://m.media-amazon.com/images/I/81AchGi9-EL._SL1361_.jpg",
+        img: allStar,
         price: 19
     },
     {
         id: "6234567",
         title: "Returnal - PS5 ",
-        img: "https://m.media-amazon.com/images/I/81bYFHhtPEL._SL1500_.jpg",
+        img: returnal,
         price: 59
     },
     {
         id: "6234567",
         title: "Ratchet & Clank: Rift Apart Launch",
-        img: "https://m.media-amazon.com/images/I/81qmSC6-FpS._SL1500_.jpg",
+        img: ratchet,
         price: 69
     },
     {
         id: "4234567",
         title: "MLB The Show 21 - PS5",
-        img: "https://m.media-amazon.com/images/I/91oRdwPcfKL._SL1500_.jpg",
+        img: mlb,
         price: 69
     },
     {
         id: "5234567",
         title: "Marvel's Spider-Man",
-        img: "https://m.media-amazon.com/images/I/71bqboqDUiL._SL1361_.jpg",
+        img: spider,
         price: 69
     }
 ]

db066d6f45f6a49f223340522180c08ea16b86c8
shayan-developer <shayan.dev.arak@gmail.com>
2021-08-27 18:19:45 +0430

    optimizing checkout

--- src/components/CheckoutProduct/index.js
+++ src/components/CheckoutProduct/index.js
@@ -1,26 +1,17 @@
 import React from 'react'
 import styles from "../../styles/CheckoutProduct.module.css"
-import { useStateValue } from '../Context/StateProvider'
 import { useTranslation } from "react-i18next";
 import { toman } from "../../lib/Totoman";
 import Button from '../Button';
-function CheckoutProduct(props) {
-    const { img, id, title, price, amount }=props
-    const state = useStateValue()
-    const [, dispath] = state
+function CheckoutProduct({ img, id, title, price, amount, minesBasket, addToBasket }) {
     const { t, i18n } = useTranslation()
     const lang = i18n.language
-    const addToBasket = () => {
-        dispath({
-            type: "ADD",
-            item: { id, title, price, amount, img }
-        })
+
+    const addHandler = () => {
+        addToBasket({ id, title, price, amount, img })
     }
-    const minesBasket = () => {
-        dispath({
-            type: "REMOVE",
-            item: id
-        })
+    const minesHandler = () => {
+        minesBasket(id)
     }
     return (
         <div className={styles.checkoutProduct} >
@@ -30,12 +21,12 @@ function CheckoutProduct(props) {
                 <p className={styles.price}> {t("price")} : {lang === "fa" ? toman(price) : ` $ ${price}`}</p>
                 <p className={styles.price}>{t("amount")} :  {amount} </p>
                 <div className={styles.actions}>
-                    <Button varient="outline" style={{ marginLeft: "8px", width: "60px" }} onClick={addToBasket}> + </Button>
-                    <Button varient="outline" style={{ marginLeft: "8px", width: "60px" }} onClick={minesBasket}> - </Button>
+                    <Button varient="outline" style={{ marginLeft: "8px", width: "60px" }} onClick={addHandler}> + </Button>
+                    <Button varient="outline" style={{ marginLeft: "8px", width: "60px" }} onClick={minesHandler}> - </Button>
                 </div>
             </div>
         </div>
     )
 }
 
-export default CheckoutProduct
+export default React.memo(CheckoutProduct)
--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import styles from "../../styles/Checkout.module.css"
 import { useStateValue } from '../../components/Context/StateProvider'
 import Subtotal from '../../components/Subtotal'
@@ -9,37 +9,57 @@ import { useTranslation } from "react-i18next";
 import { Helmet } from 'react-helmet'
 import Layout from "../../components/Layout"
 function Checkout() {
-    const state = useStateValue()
-    const { basket } = state[0]
+    const [state, dispath] = useStateValue()
+    const { basket } = state
     const { t } = useTranslation()
+    const addToBasket = useCallback(data => {
+        dispath({
+            type: "ADD",
+            item: data
+        })
+    }, [dispath])
+    const minesBasket = useCallback(id => {
+        dispath({
+            type: "REMOVE",
+            item: id
+        })
+    }, [dispath])
     return (
-    <Layout>
-        <Helmet>
-            <title> {t("shopping_bas")} </title>
-        </Helmet>
-        <Row className={styles.row}>
-            <Col span={24} className={styles.checkout}>
-                <div className={styles.calc}>
-                    <Subtotal />
-                </div>
-                <div className={styles.items}>
-                    <p className={styles.title}>{t("your_basket")} </p>
-                </div>
-                {
-                    basket.length >= 1 ? basket.map(item => {
-                        const { img, id, title, price, amount }=item
-                  return  <CheckoutProduct img={img} price={price} id={id} amount={amount} title={title} key={id} />
-                }) :
-                        <div className={styles.empty}>
-                            <div className={styles.icon}>
-                                <MdSentimentDissatisfied />
+        <Layout>
+            <Helmet>
+                <title> {t("shopping_bas")} </title>
+            </Helmet>
+            <Row className={styles.row}>
+                <Col span={24} className={styles.checkout}>
+                    <div className={styles.calc}>
+                        <Subtotal />
+                    </div>
+                    <div className={styles.items}>
+                        <p className={styles.title}>{t("your_basket")} </p>
+                    </div>
+                    {
+                        basket.length >= 1 ? basket.map(item => {
+                            const { img, id, title, price, amount } = item
+                            return <CheckoutProduct
+                                minesBasket={minesBasket}
+                                addToBasket={addToBasket}
+                                img={img}
+                                price={price}
+                                id={id}
+                                amount={amount}
+                                title={title}
+                                key={id} />
+                        }) :
+                            <div className={styles.empty}>
+                                <div className={styles.icon}>
+                                    <MdSentimentDissatisfied />
+                                </div>
+                                {t("empty_basket")}
                             </div>
-                            {t("empty_basket")}
-                        </div>
-                }
-            </Col>
-        </Row>
-    </Layout>
+                    }
+                </Col>
+            </Row>
+        </Layout>
     )
 }
 
